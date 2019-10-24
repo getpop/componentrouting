2c9d3588ea475c181d1f50ad5498a8c10a90d4a7
Leo <losoviz@gmail.com>
2019-10-25 00:22:44 +0800

    Fixed bug

--- src/Utils.php
+++ src/Utils.php
@@ -9,7 +9,7 @@ class Utils {
 
     // This function is an implementation of a recursive `array_intersect_assoc`, so that in the RouteModuleProcessor we can ask for conditions recursively (eg: array('routing-state' => array('post-type' => 'event')))
     // Modified from https://stackoverflow.com/questions/4627076/php-question-how-to-array-intersect-assoc-recursively
-    public static function arrayIntersectAssocRecursive(array &$arr1, array &$arr2): array
+    public static function arrayIntersectAssocRecursive(&$arr1, &$arr2): array
     {
         if (!is_array($arr1) || !is_array($arr2)) {
             return (string) $arr1 == (string) $arr2 ? $arr1 : null;
