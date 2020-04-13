ef6a59b08973c5d9794e6668bbda5a17032f77f0
Leo <losoviz@gmail.com>
2020-04-13 19:17:26 +0800

    Added declare(strict_types=1);

--- src/AbstractEntryRouteModuleProcessor.php
+++ src/AbstractEntryRouteModuleProcessor.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\ModuleRouting;
 
 abstract class AbstractEntryRouteModuleProcessor extends AbstractRouteModuleProcessor
--- src/Facades/RouteModuleProcessorManagerFacade.php
+++ src/Facades/RouteModuleProcessorManagerFacade.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\ModuleRouting\Facades;
 
 use PoP\ModuleRouting\RouteModuleProcessorManagerInterface;
